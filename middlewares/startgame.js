cde08b543c65838796c84e812589656b17411464
parthnamdev <parthnamdev@users.noreply.github.com>
2021-05-14 17:56:29 +0530

    bug

--- middlewares/startgame.js
+++ middlewares/startgame.js
@@ -3,8 +3,10 @@ const startgame = (req, res, next) => {
     let toStartTime = new Date('05/14/2021 05:45:00 PM');
     let endtime = new Date('05/14/2021 06:50:00 PM');
     if( current > toStartTime && current < endtime) {
+        console.log("yes");
         next();
     } else {
+        console.log("no");
         res.render('treasureHunt/hunt_index', {alert: 'Game time is 10:15 to 11:00 pm'})
     }
 }
