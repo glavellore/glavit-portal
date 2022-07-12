ad5635e39270fd829d96f8e150645f76b82a2cb9
sairamesh1642 <sairamesh1642@gmail.com>
2022-07-12 19:22:00 +0530

    bug fixes

--- public/captureFlag/script.js
+++ public/captureFlag/script.js
@@ -2,17 +2,17 @@ var que = document.getElementById('question').value;
 
 var ansInput = document.getElementById('answerInput');
 var ans;
-ansInput.addEventListener('keyup', function() {
-    ans = ansInput.value;
+ansInput.addEventListener('keyup', function () {
+  ans = ansInput.value;
 });
 
 const startgameBtn = document.getElementById('startgame');
 
 let now = Date.now();
-let toStart = new Date(process.env.START);
-let toEnd = new Date(process.env.END);
+let toStart = new Date('07/12/2022 06:00:00 PM');
+let toEnd = new Date('07/12/2022 08:00:00 PM');
 
-if( now > toStart && now < toEnd) {
+if (now > toStart && now < toEnd) {
   startgameBtn.disabled = false;
   startgameBtn.innerText = "Enter event";
 } else {
@@ -30,7 +30,7 @@ submitBtn.disabled = true;
 const timer = document.getElementById('timer');
 
 
-let endtime = new Date(process.env.END).getTime();
+let endtime = new Date('07/12/2022 08:00:00 PM').getTime();
 
 
 
@@ -40,23 +40,23 @@ var trivia_timer = setInterval(Timer, 1000);
 
 
 
-function Timer(){ 
+function Timer() {
   let current = new Date().getTime();
 
   let diff = endtime - current;
 
-  let hour = Math.floor( (diff % (1000*60*60*24)) / (1000*60*60) );
-  let min = Math.floor( (diff % (1000*60*60)) / (1000*60) );
-  let sec = Math.floor( (diff % (1000*60)) / (1000) );
+  let hour = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let min = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  let sec = Math.floor((diff % (1000 * 60)) / (1000));
 
-  timer.innerText = hour + ":" + min + ":" + sec; 
-  
-  if(hour <= 0 && min <= 0 && sec <= 0) {
+  timer.innerText = hour + ":" + min + ":" + sec;
+
+  if (hour <= 0 && min <= 0 && sec <= 0) {
     stopTimer();
     timer.innerText = "00:00:00";
 
     ansInput.value = 'not answered';
-  
+
     let tosSubmit = document.getElementById('hunt-form');
     tosSubmit.submit();
 
@@ -68,45 +68,45 @@ function stopTimer() {
 }
 
 function closeAlert(id) {
-    document.getElementById(id).style.display = 'none';
+  document.getElementById(id).style.display = 'none';
 }
 
 
 function requestServer() {
-        fetch(`/capture/checkAnswer/${que}/${ans}`)
-        .then(result => result.json())
-        .then(result => {
-        // console.log(result.cluemein);
+  fetch(`/capture/checkAnswer/${que}/${ans}`)
+    .then(result => result.json())
+    .then(result => {
+      // console.log(result.cluemein);
 
-        switch(result.cluemein) {
-            case 0:
-              wrongAnsAlert.style.display = "inline";
-              rightAnsAlert.style.display = "none";
-              errorAnsAlert.style.display = "none";
-              submitBtn.disabled = true;
-              break;
-            case 1:
-              rightAnsAlert.style.display = "inline";
-              wrongAnsAlert.style.display = "none";
-              errorAnsAlert.style.display = "none";
-              submitBtn.disabled = false;
-              break;
-            case -1:
-                errorAnsAlert.style.display = "inline";
-                rightAnsAlert.style.display = "none";
-                wrongAnsAlert.style.display = "none";
-                submitBtn.disabled = true;
-              break;
-            default:
-                errorAnsAlert.style.display = "inline";
-                rightAnsAlert.style.display = "none";
-                wrongAnsAlert.style.display = "none";
-                submitBtn.disabled = true;
-          }
-        })
-        .catch(err => {
-            console.log(err);
-        })
+      switch (result.cluemein) {
+        case 0:
+          wrongAnsAlert.style.display = "inline";
+          rightAnsAlert.style.display = "none";
+          errorAnsAlert.style.display = "none";
+          submitBtn.disabled = true;
+          break;
+        case 1:
+          rightAnsAlert.style.display = "inline";
+          wrongAnsAlert.style.display = "none";
+          errorAnsAlert.style.display = "none";
+          submitBtn.disabled = false;
+          break;
+        case -1:
+          errorAnsAlert.style.display = "inline";
+          rightAnsAlert.style.display = "none";
+          wrongAnsAlert.style.display = "none";
+          submitBtn.disabled = true;
+          break;
+        default:
+          errorAnsAlert.style.display = "inline";
+          rightAnsAlert.style.display = "none";
+          wrongAnsAlert.style.display = "none";
+          submitBtn.disabled = true;
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    })
 
 }
 
